e07c0f52175130183ffb216f15b43416c554ae51
Eloy Holzgrefe <eloyneto61@gmail.com>
2023-11-14 14:42:31 -0300

    Create einstein.py

---
+++ projetos-cs50/einstein.py
@@ -0,0 +1,13 @@
+def calc_emc(): 
+
+    print ("Welcome to the einstein brain simulator")
+    print ("Discorver How much of enerngy you cam generate with:" )
+
+    c= int(300000000)
+    m = int(input ("m: "))
+    E = m * (c * c)
+
+    print ("E =",E,"J")
+
+
+calc_emc()
